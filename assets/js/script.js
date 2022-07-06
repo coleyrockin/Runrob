f44885459660ebaee875d321292851f028b20313
Ruminert <ruminertt@Tanners-MacBook-Air.local>
2022-07-06 15:41:10 -0500

    first fetch request

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var getNutrition = function(food){
+    var apiUrl = "https://api.edamam.com/api/nutrition-data?app_id=34642c65&app_key=d86fd599cef056ad8909daacaab63cf2&nutrition-type=logging&ingr=white-rice";
+     fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+                console.log(data);
+
+        });
+    });
+};
+
+        getNutrition();
