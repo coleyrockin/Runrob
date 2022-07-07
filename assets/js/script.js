67948a825a5ea0c8d764ae4c6bd39b646fb5ac34
coleyrockin <coleyrockin@aol.com>
2022-07-06 21:12:03 -0500

    uh oh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var foodEl = document.querySelector("#food")
+
 var getNutrition = function(food){
     var apiUrl = "https://api.edamam.com/api/nutrition-data?app_id=34642c65&app_key=d86fd599cef056ad8909daacaab63cf2&nutrition-type=logging&ingr=white-rice";
      fetch(apiUrl).then(function(response){
