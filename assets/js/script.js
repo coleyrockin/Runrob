2dd40cc9fc94be11ad7500f2b0c59d4eb496b6f3
Ruminert <ruminertt@tanners-air.attlocal.net>
2022-07-06 21:46:07 -0500

    fixed text content display

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,14 @@ var foodEl = document.querySelector("#food");
 
 
 var getNutrition = function(food){
+    var foodDiv = document.createElement("div");
+
     var apiUrl = "https://api.edamam.com/api/nutrition-data?app_id=34642c65&app_key=d86fd599cef056ad8909daacaab63cf2&nutrition-type=logging&ingr=" + food;
      fetch(apiUrl).then(function(response){
          response.json().then(function(data){
+             var foodData = data.totalNutrients.PROCNT.quantity;
+            foodDiv.textContent = foodData;
+            foodEl.appendChild(foodDiv);
                console.log(data);
 
         });
@@ -16,11 +21,7 @@ var getNutrition = function(food){
 
 var getFood = function(event){
     event.preventDefault()
-    var foodDiv = document.createElement("div");
     var food = inputEl.value.trim();
-    
-    foodDiv.textContent = foodData;
-    foodEl.appendChild(foodDiv);
     getNutrition(food);
 };
 buttonEl.addEventListener("click", getFood);
