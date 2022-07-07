87cb6981f00475182e7c11921096d91128282574
Ruminert <ruminertt@tanners-air.attlocal.net>
2022-07-06 23:40:03 -0500

    background and basic css

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ var getNutrition = function(food){
     var apiUrl = "https://api.edamam.com/api/nutrition-data?app_id=34642c65&app_key=d86fd599cef056ad8909daacaab63cf2&nutrition-type=logging&ingr=" + food;
      fetch(apiUrl).then(function(response){
          response.json().then(function(data){
-             var foodData = data.totalNutrients.PROCNT.quantity;
+             var foodData =  data.totalNutrients.PROCNT.quantity +  data.totalNutrients.PROCNT.unit + " of " + data.totalNutrients.PROCNT.label  + " per " + data.totalWeight + " grams";
             foodDiv.textContent = foodData;
             foodEl.appendChild(foodDiv);
                console.log(data);
