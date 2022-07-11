5b8bb2accdec2e04270f20f241c5035170a985e3
Ruminert <ruminertt@tanners-air.attlocal.net>
2022-07-10 20:28:34 -0500

    more nonsense

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,9 @@ var inputEl = document.querySelector("#input");
 var foodEl = document.querySelector("#food");
 
 var getNutrition = function(food){
-<<<<<<< HEAD
-<<<<<<< HEAD
-    
 
-
-    var apiUrl = "https://api.edamam.com/api/nutrition-data?app_id=34642c65&app_key=d86fd599cef056ad8909daacaab63cf2&nutrition-type=logging&ingr=" + food;
+     var apiUrl = "https://api.edamam.com/api/nutrition-data?app_id=34642c65&app_key=d86fd599cef056ad8909daacaab63cf2&nutrition-type=logging&ingr=" + food;
+     
      fetch(apiUrl).then(function(response){
          response.json().then(function(data){
              var foodData =  (data.totalNutrients.PROCNT.quantity / data.calories).toFixed(2) +  data.totalNutrients.PROCNT.unit + " of " + data.totalNutrients.PROCNT.label  + " per calorie";
@@ -37,40 +34,10 @@ var getNutrition = function(food){
     
             foodEl.appendChild(foodHeader);
                console.log(data);
-=======
-=======
->>>>>>> feature/tanner
-    var apiUrl = "https://api.edamam.com/api/nutrition-data?app_id=34642c65&app_key=d86fd599cef056ad8909daacaab63cf2&nutrition-type=logging&ingr=" + food;
-    fetch(apiUrl).then(function(response){
-        response.json().then(function(data){
-            var foodData = (data.totalNutrients.PROCNT.quantity / data.calories).toFixed(2) +  data.totalNutrients.PROCNT.unit + " of " + data.totalNutrients.PROCNT.label  + " per calorie";
-             
-            var foodHeader = document.createElement("div");
-            foodHeader.setAttribute("class", "card-header");
-            foodHeader.classList = "mt-3"
-         
-            
-            var foodTitle = document.createElement("div");
-            foodTitle.setAttribute("class", "card-header-title");
-            foodTitle.textContent = food;
-             
-            var foodP= document.createElement("p");
-            foodP.setAttribute("class", "card-content");
-            foodP.textContent = foodData;
-             
-            foodHeader.appendChild(foodTitle);
-            foodTitle.appendChild(foodP);
-            
-            foodEl.appendChild(foodHeader);
-            console.log(data);
-<<<<<<< HEAD
->>>>>>> 756edd1c228976b668876dd22405f8299fd7850b
-=======
->>>>>>> feature/tanner
+         })
+        })
+    }
 
-        });
-    });
-};
 
 var getProductPrice = function(foodPrice){
     var apiUrl2 = "https://api.redcircleapi.com/request?api_key=8F36CF55536D4A5E85C6E3625ECB6127&type=search&search_term=" + foodPrice + "&sort_by=best_match";
