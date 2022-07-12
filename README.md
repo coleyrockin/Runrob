95c3f27ec2404a9de3f043687b2c9b284cbc895a
coleyrockin <coleyrockin@aol.com>
2022-07-11 20:59:05 -0500

    Comments,Readme update, Bulma updates, responsive issues solved, Codecleaned up, added comments

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
-# Runrob
-Collaborative Project 
+Runrob, AKA Nutri Wizzard was starting name behind the most secretive, innovative, mind bending project in the last century.
 
-* Use a CSS framework other than Bootstrap.
-* Be deployed to GitHub Pages.
-* Be interactive (i.e., accept and respond to user input).
-* Use at least two server-side APIs.
-* Does not use alerts, confirms, or prompts (use modals).
-* Use client-side storage to store persistent data.
-* Be responsive.
-* Have a polished UI.
-* Have a clean repository that meets quality coding standards (file structure, naming conventions, follows best practices for class/id naming conventions, indentation, quality comments, etc.).
-* Have a quality README (with unique name, description, technologies used, screenshot, and link to deployed application).
-Finally, you must add your project to the portfolio that you created in Module 2.
+Collaborative Project by Tanner Runiner and Boyd Roberts.
+https://github.com/T-rummy -Tanner R
+https://github.com/coleyrockin -Boyd R
+
+https://github.com/coleyrockin/Runrob -Repository
+https://coleyrockin.github.io/Runrob/ -Deployed Application
+
+Project Requirements
+1. Use a CSS framework other than Bootstrap - (We used https://bulma.io/ **)
+2. Be deployed to GitHub Pages.
+3. Be interactive (i.e., accept and respond to user input).
+4. Use at least two server-side APIs. (Used Edamam and Redcircle API **)
+5. Does not use alerts, confirms, or prompts (use modals).
+6. Use client-side storage to store persistent data.
+7. Be responsive.
+8. Have a polished UI.
+9. Have a clean repository that meets quality coding standards (file structure, naming conventions, follows best practices for class/id naming conventions, indentation, quality comments, etc.).
+10. Have a quality README (with unique name, description, technologies used, screenshot, and link to deployed application).
+11. Finally, you must add your project to the portfolio that you created in Module 2.
+
+[!img](./assets/images/) 
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,60 +2,48 @@ var buttonEl = document.querySelector("#searchBtn");
 var inputEl = document.querySelector("#input");
 var foodEl = document.querySelector("#food");
 
+// First API EDAMAM getting nutritonal facts - Protein, Carbs, Fat
 var getNutrition = function(food){
-
-     var apiUrl = "https://api.edamam.com/api/nutrition-data?app_id=34642c65&app_key=d86fd599cef056ad8909daacaab63cf2&nutrition-type=logging&ingr=" + food;
-     
-     fetch(apiUrl).then(function(response){
-         response.json().then(function(data){
-             var foodData1 =  (data.totalNutrients.PROCNT.quantity / data.calories).toFixed(2) +  data.totalNutrients.PROCNT.unit + " of " + data.totalNutrients.PROCNT.label  + " per calorie";
-             var foodData2 = (data.totalNutrients.CHOCDF.quantity / data.calories).toFixed(2) +  data.totalNutrients.CHOCDF.unit + " of " + data.totalNutrients.CHOCDF.label  + " per calorie";
-             var foodData3 = (data.totalNutrients.FAT.quantity / data.calories).toFixed(2) +  data.totalNutrients.FAT.unit + " of " + data.totalNutrients.FAT.label  + " per calorie";
-             
-             var foodHeader = document.createElement("div");
-             foodHeader.setAttribute("class", "card-header");
-             foodHeader.classList = "mt-3"
+    var apiUrl = "https://api.edamam.com/api/nutrition-data?app_id=34642c65&app_key=d86fd599cef056ad8909daacaab63cf2&nutrition-type=logging&ingr=" + food;
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+        var foodData1 =  (data.totalNutrients.PROCNT.quantity / data.calories).toFixed(2) +  data.totalNutrients.PROCNT.unit + " of " + data.totalNutrients.PROCNT.label  + " per calorie";
+        var foodData2 = (data.totalNutrients.CHOCDF.quantity / data.calories).toFixed(2) +  data.totalNutrients.CHOCDF.unit + " of " + data.totalNutrients.CHOCDF.label  + " per calorie";
+        var foodData3 = (data.totalNutrients.FAT.quantity / data.calories).toFixed(2) +  data.totalNutrients.FAT.unit + " of " + data.totalNutrients.FAT.label  + " per calorie";
+            
+        var foodHeader = document.createElement("div");
+        foodHeader.setAttribute("class", "card-header");
+        foodHeader.classList = "mt-3"
          
+        var foodTitle = document.createElement("div");
+        foodTitle.setAttribute("class", "card-header-title");
+        foodTitle.textContent = food;
             
-             var foodTitle = document.createElement("div");
-             foodTitle.setAttribute("class", "card-header-title");
-             foodTitle.textContent = food;
-             
-             var foodP1= document.createElement("p");
-             foodP1.setAttribute("class", "card-content");
-             foodP1.textContent = foodData1;
+        var foodP1= document.createElement("p");
+        foodP1.setAttribute("class", "card-content");
+        foodP1.textContent = foodData1;
 
-             var foodP2= document.createElement("p");
-             foodP2.setAttribute("class", "card-content");
-             foodP2.textContent = foodData2;
-
-             var foodP3= document.createElement("p");
-             foodP3.setAttribute("class", "card-content");
-             foodP3.textContent = foodData3;
+        var foodP2= document.createElement("p");
+        foodP2.setAttribute("class", "card-content");
+        foodP2.textContent = foodData2;
+        
+        var foodP3= document.createElement("p");
+        foodP3.setAttribute("class", "card-content");
+        foodP3.textContent = foodData3;
              
-             
-             
-             
-            
-             
-             foodHeader.appendChild(foodTitle);
-             foodTitle.appendChild(foodP1);
-             foodTitle.appendChild(foodP2);
-             foodTitle.appendChild(foodP3);
-            
-             
-    
-            foodEl.appendChild(foodHeader);
-               console.log(data);
-         })
+        foodHeader.appendChild(foodTitle);
+        foodTitle.appendChild(foodP1);
+        foodTitle.appendChild(foodP2);
+        foodTitle.appendChild(foodP3);
+        foodEl.appendChild(foodHeader);
+        console.log(data);
         })
-    }
+    })
+};
 
-
+// Second API REDCIRCLE getting price information on top four best matched products then averaging the price.
 var getProductPrice = function(foodPrice){
     var apiUrl2 = "https://api.redcircleapi.com/request?api_key=8F36CF55536D4A5E85C6E3625ECB6127&type=search&search_term=" + foodPrice + "&sort_by=best_match";
-
-
     fetch(apiUrl2).then(function(response){
         response.json().then(function(data){
         console.log(data)
@@ -68,7 +56,7 @@ var getProductPrice = function(foodPrice){
 
         })
     })
-}
+};
 
 var getPrice= function(event){
     event.preventDefault()
@@ -81,5 +69,6 @@ var getFood = function(event){
     var food = inputEl.value.trim();
     getNutrition(food);
 };
+
 buttonEl.addEventListener("click", getFood);
 buttonEl.addEventListener("click", getPrice);
