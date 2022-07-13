032e40d2a8160af068dbfefd5705ac86fb4aa37a
coleyrockin <coleyrockin@aol.com>
2022-07-12 20:57:40 -0500

    Clean

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,64 +2,41 @@ var buttonEl = document.querySelector("#searchBtn");
 var inputEl = document.querySelector("#input");
 var foodEl = document.querySelector("#food");
 
-
-
-
 if (localStorage.getItem("FoodInfo1")){
     var foodHeader = document.createElement("div");
     foodHeader.setAttribute("class", "card-header");
     foodHeader.classList = "mt-3"
-     
-
-        
-         var foodTitle = document.createElement("div");
-         foodTitle.setAttribute("class", "card-header-title");
-         var input0 = localStorage.getItem("FoodInfo1");
-         foodTitle.textContent = input0;
+    
+    var foodTitle = document.createElement("div");
+    foodTitle.setAttribute("class", "card-header-title");
+    var input0 = localStorage.getItem("FoodInfo1");
+    foodTitle.textContent = input0;
          
+    var foodP1= document.createElement("p");
+    foodP1.setAttribute("class", "card-content");
+    var input1 = localStorage.getItem("FoodInfo2");
+    foodP1.textContent = input1;
          
-         var foodP1= document.createElement("p");
-         foodP1.setAttribute("class", "card-content");
-         var input1 = localStorage.getItem("FoodInfo2");
-         foodP1.textContent = input1;
+    var foodP2= document.createElement("p");
+    foodP2.setAttribute("class", "card-content");
+    var input2 = localStorage.getItem("FoodInfo3");
+    foodP2.textContent = input2;
          
+    var foodP3= document.createElement("p");
+    foodP3.setAttribute("class", "card-content");
+    var input3 = localStorage.getItem("FoodInfo4");
+    foodP3.textContent = input3;
 
-         var foodP2= document.createElement("p");
-         foodP2.setAttribute("class", "card-content");
-         var input2 = localStorage.getItem("FoodInfo3");
-         foodP2.textContent = input2;
-         
-
-         var foodP3= document.createElement("p");
-         foodP3.setAttribute("class", "card-content");
-         var input3 = localStorage.getItem("FoodInfo4");
-         foodP3.textContent = input3;
-
-         var priceEl = document.createElement("p");
-         priceEl.setAttribute("class", "card-content")
-         var input5 = localStorage.getItem("foodPrice");
-         priceEl.textContent = input5;
+    var priceEl = document.createElement("p");
+    priceEl.setAttribute("class", "card-content")
+    var input5 = localStorage.getItem("foodPrice");
+    priceEl.textContent = input5;
  
-         
-         
-         
-         
-         
-         
-        
-         
-         foodHeader.appendChild(foodTitle);
-         foodTitle.appendChild(foodP1);
-         foodTitle.appendChild(foodP2);
-         foodTitle.appendChild(foodP3);
-        
-         
     foodHeader.appendChild(foodTitle);
     foodTitle.appendChild(foodP1);
     foodTitle.appendChild(foodP2);
     foodTitle.appendChild(foodP3);
     foodEl.appendChild(foodHeader);
-
     foodEl.appendChild(priceEl);
   
 }
@@ -76,52 +53,40 @@ var getNutrition = function(food){
         var foodHeader = document.createElement("div");
         foodHeader.setAttribute("class", "card-header");
         foodHeader.classList = "mt-3"
-         
-    
-            
-             var foodTitle = document.createElement("div");
-             foodTitle.setAttribute("class", "card-header-title");
-             foodTitle.textContent = food;
-             localStorage.setItem("FoodInfo1" , food);
-             var input0 = localStorage.getItem("FoodInfo1");
-             foodTitle.textContent = input0;
+        
+        var foodTitle = document.createElement("div");
+        foodTitle.setAttribute("class", "card-header-title");
+        foodTitle.textContent = food;
+        localStorage.setItem("FoodInfo1" , food);
+        var input0 = localStorage.getItem("FoodInfo1");
+        foodTitle.textContent = input0;
              
+        var foodP1= document.createElement("p");
+        foodP1.setAttribute("class", "card-content");
+        foodP1.textContent = foodData1;
+        localStorage.setItem("FoodInfo2" , foodData1 );
+        var input1 = localStorage.getItem("FoodInfo2");
+        foodP1.textContent = input1;
+        
+        var foodP2= document.createElement("p");
+        foodP2.setAttribute("class", "card-content");
+        foodP2.textContent = foodData2;
+        localStorage.setItem("FoodInfo3" , foodData2);
+        var input2 = localStorage.getItem("FoodInfo3");
+        foodP2.textContent = input2;
              
-             var foodP1= document.createElement("p");
-             foodP1.setAttribute("class", "card-content");
-             foodP1.textContent = foodData1;
-             localStorage.setItem("FoodInfo2" , foodData1 );
-             var input1 = localStorage.getItem("FoodInfo2");
-             foodP1.textContent = input1;
-             
-
-             var foodP2= document.createElement("p");
-             foodP2.setAttribute("class", "card-content");
-             foodP2.textContent = foodData2;
-             localStorage.setItem("FoodInfo3" , foodData2);
-             var input2 = localStorage.getItem("FoodInfo3");
-             foodP2.textContent = input2;
-             
-
-             var foodP3= document.createElement("p");
-             foodP3.setAttribute("class", "card-content");
-             foodP3.textContent = foodData3;
-             localStorage.setItem("FoodInfo4" ,  foodData3);
-             var input3 = localStorage.getItem("FoodInfo4");
-             foodP3.textContent = input3;
-             
-             
-             
-             
-             
-            
+        var foodP3= document.createElement("p");
+        foodP3.setAttribute("class", "card-content");
+        foodP3.textContent = foodData3;
+        localStorage.setItem("FoodInfo4" ,  foodData3);
+        var input3 = localStorage.getItem("FoodInfo4");
+        foodP3.textContent = input3;
              
         foodHeader.appendChild(foodTitle);
         foodTitle.appendChild(foodP1);
         foodTitle.appendChild(foodP2);
         foodTitle.appendChild(foodP3);
-            
-             
+        
         foodHeader.appendChild(foodTitle);
         foodTitle.appendChild(foodP1);
         foodTitle.appendChild(foodP2);
@@ -163,8 +128,6 @@ var getFood = function(event){
     var food = inputEl.value.trim();
     getNutrition(food);
 };
-
-
 
 buttonEl.addEventListener("click", getFood);
 buttonEl.addEventListener("click", getPrice);
